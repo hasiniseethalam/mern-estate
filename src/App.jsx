1f718a287b29435d16b9a4ebf310337b1003c877
hasini <seethalamchandrahasini@gmail.com>
2024-04-15 13:58:12 +0530

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ import SignIn from "./pages/SignIn";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Home from "./pages/Home";
+import Header from "./components/Header";
 
 export default function App() {
-  return <BrowserRouter>
+  return (
+  <BrowserRouter>
+  <Header/>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/sign-in" element={<SignIn />} />
@@ -15,7 +18,7 @@ export default function App() {
       <Route path="/profile" element={<Profile />} />
     </Routes>
   </BrowserRouter>
-  
+  );
   
 }
 
